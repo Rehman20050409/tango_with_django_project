135e6a2ab8b761d63a895c160046bc3adf176d39
Rehman20050409 <2765081k@student.gla.ac.uk>
2025-02-12 13:30:30 +0000

    Update urls.py

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # Django admin panel
 ]
 
-# ✅ Serve media files only in development (DEBUG=True)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
