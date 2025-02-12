e3da90fdc752350a021cce37877e7764fabc2a5b
Rehman20050409 <2765081k@student.gla.ac.uk>
2025-02-12 11:19:44 +0000

    Update settings.py

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -118,5 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATICFILES_DIRS = [STATIC_DIR]
-STATIC_URL = '/static/,images/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Corrected
+STATIC_URL = '/static/'
